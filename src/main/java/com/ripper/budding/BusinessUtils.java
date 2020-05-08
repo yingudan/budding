c6297e12fd8f60ee3552f4b62c36bea767448c03
ripper <ripper@DESKTOP-3NSPIBB>
2020-05-08 10:08:21 +0800

    新增减法和除法

--- src/main/java/com/ripper/budding/BusinessUtils.java
+++ src/main/java/com/ripper/budding/BusinessUtils.java
@@ -46,4 +46,41 @@ public class BusinessUtils {
 		return a.add(b);
 	}
 
+	/**
+	 * 减法 止空对象报错
+	 * 
+	 * @return a+b
+	 * @author ripper [RipperF@hotmail.com]
+	 * @date 2020年1月21日
+	 */
+	public static final BigDecimal sub(BigDecimal a, BigDecimal b) {
+		if (a == null) {
+			a = BigDecimal.ZERO;
+		}
+		if (b == null) {
+			b = BigDecimal.ZERO;
+		}
+		return a.subtract(b);
+	}
+
+	/**
+	 * 除法保留小数
+	 * 
+	 * @return a/b
+	 * @author ripper [RipperF@hotmail.com]
+	 * @date 2020年1月21日
+	 */
+	public static final BigDecimal divide(BigDecimal a, BigDecimal b, Integer theNum) {
+		if (a == null) {
+			return BigDecimal.ZERO;
+		}
+		if (b == null) {
+			return BigDecimal.ZERO;
+		}
+		if (BigDecimal.ZERO.compareTo(b) == 0) {
+			return BigDecimal.ZERO;
+		}
+		return a.divide(b, theNum, BigDecimal.ROUND_HALF_UP);
+	}
+
 }
